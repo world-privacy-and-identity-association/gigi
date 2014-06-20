515007c7c1351045420669d65b59c08fa46850f2
Benny Baumann <BenBE@geshi.org>
2014-06-20 19:59:47 +0200

    add: Script for retrieving a recent version of Jetty

---
+++ doc/getJetty.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+JETTY=C:/jars/jetty-distribution-9.1.0.RC0/org.eclipse.jetty.project
+
+pushd ../lib/jetty/org/eclipse/jetty
+rm -fR *
+
+pushd $JETTY
+
+git checkout refs/tags/jetty-9.2.1.v20140609
+popd
+
+
+for package in http io security server servlet util
+do
+    cp -R $JETTY/jetty-$package/src/main/java/org/eclipse/jetty/$package .
+done
+
+cp -R $JETTY/jetty-http/src/main/resources/org/eclipse/jetty/http .
+
+
+
+cp -R $JETTY/jetty-$package/src/main/java/org/eclipse/jetty/$package .
+
+rm -R server/session/jmx
+rm -R server/handler/jmx
+rm -R server/jmx
+rm -R servlet/jmx
+
+rm util/log/JettyAwareLogger.java
+rm util/log/Slf4jLog.java
+rm server/Slf4jRequestLog.java
+popd
