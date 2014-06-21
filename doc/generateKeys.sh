91305af8e77fd4a3e5d093de825b49d34f4944fa
Felix Dörre <felix@dogcraft.de>
2014-06-21 21:46:56 +0200

    Update keys scripts
    -nodes doesnt exist for "genrsa"
    import own certificate in truststore

--- doc/generateKeys.sh
+++ doc/generateKeys.sh
@@ -1,6 +1,6 @@
 # this script generates a simple self-signed keypair
 
-openssl genrsa -nodes -out jetty.key 4096
+openssl genrsa -out jetty.key 4096
 openssl req -new -key jetty.key -out jetty.csr -subj "/CN=localhost" -config selfsign.config
 openssl x509 -req -days 365 -in jetty.csr -signkey jetty.key -out jetty.crt
 openssl pkcs12 -inkey jetty.key -in jetty.crt -export -passout pass: -out ../config/keystore.pkcs12
--- doc/generateTruststore.sh
+++ doc/generateTruststore.sh
@@ -1,11 +1,10 @@
 # this script generates a simple self-signed keypair
 
-wget http://www.cacert.org/certs/root.crt
-wget http://www.cacert.org/certs/class3.crt
+wget -N http://www.cacert.org/certs/root.crt
+wget -N http://www.cacert.org/certs/class3.crt
 
 keytool -importcert -keystore ../config/cacerts.jks -file root.crt -alias root -storepass "changeit"
 keytool -importcert -keystore ../config/cacerts.jks -file class3.crt -alias class3 -storepass "changeit"
-rm root.crt
-rm class3.crt
+keytool -importcert -keystore ../config/cacerts.jks -file jetty.crt -alias own -storepass "changeit"
 
 keytool -list -keystore ../config/cacerts.jks -storepass "changeit"
