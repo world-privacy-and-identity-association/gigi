81e74e9b8509d52b72c3315717c0940e852caffb
Janis Streib <janis@dogcraft.de>
2014-06-27 11:22:34 +0200

    Added text + more spekaing path

--- src/org/cacert/gigi/pages/account/MailAdd.java
+++ src/org/cacert/gigi/pages/account/MailAdd.java
@@ -1,17 +1,19 @@
 package org.cacert.gigi.pages.account;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.cacert.gigi.Language;
 import org.cacert.gigi.output.DataTable;
 import org.cacert.gigi.output.DataTable.Cell;
 import org.cacert.gigi.pages.Page;
 
 public class MailAdd extends Page{
-	public static final String DEFAULT_PATH = "/account/mail/new";
+	public static final String DEFAULT_PATH = "/account/mails/add";
 	public MailAdd(String title) {
 		super(title);
 	}
@@ -23,11 +25,18 @@ public class MailAdd extends Page{
 		cells.add(new Cell("Add Email", true, 2, "class=\"title\""));
 		cells.add(new Cell("Email Address", true));
 		cells.add(new Cell("<input type=\"text\" name=\"newemail\">", false));
-		String trans = getLanguage(req).getTranslation("I own or am authorised to control this email address");
+		Language language = getLanguage(req);
+		String trans = language.getTranslation("I own or am authorised to control this email address");
 		cells.add(new Cell("<input type=\"submit\" name=\"process\" value=\""
 				+ trans + "\">", false, 2));
 		DataTable dt = new DataTable(2, cells);
-		dt.output(resp.getWriter(), getLanguage(req));
+		dt.output(resp.getWriter(), language);
+		PrintWriter out = resp.getWriter();
+		out.println("<p>");
+		out.println(language
+				.getTranslation(
+						"Currently we only issue certificates for Punycode domains if the person requesting them has code signing attributes attached to their account, as these have potentially slightly higher security risk."));
+		out.println("</p>");
 	}
 
 }
