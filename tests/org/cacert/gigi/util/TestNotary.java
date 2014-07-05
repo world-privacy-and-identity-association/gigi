43a9a9b70dc1201901d333492e8efeb015a2e33c
Felix Dörre <felix@dogcraft.de>
2014-07-05 20:20:07 +0200

    Implement PoJam test.

--- tests/org/cacert/gigi/util/TestNotary.java
+++ tests/org/cacert/gigi/util/TestNotary.java
@@ -1,8 +1,10 @@
 package org.cacert.gigi.util;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import org.cacert.gigi.User;
+import org.cacert.gigi.database.DatabaseConnection;
 import org.cacert.gigi.testUtils.ManagedTest;
 import org.cacert.gigi.util.Notary.AssuranceResult;
 import org.junit.Test;
@@ -11,7 +13,7 @@ import static org.junit.Assert.*;
 
 public class TestNotary extends ManagedTest {
 	@Test
-	public void testSigning() throws SQLException {
+	public void testNormalAssurance() throws SQLException {
 		User[] users = new User[30];
 		for (int i = 0; i < users.length; i++) {
 			int id = createVerifiedUser("fn" + i, "ln" + i, createUniqueName()
@@ -42,5 +44,32 @@ public class TestNotary extends ManagedTest {
 
 		assertEquals(2 + 60, assurer.getExperiencePoints());
 
+	}
+
+	@Test
+	public void testPoJam() throws SQLException {
+		User[] users = new User[30];
+		for (int i = 0; i < users.length; i++) {
+			int id = createVerifiedUser("fn" + i, "ln" + i, createUniqueName()
+					+ "@email.org", "xvXV12°§");
+			users[i] = new User(id);
+		}
+		int id = createAssuranceUser("fn", "ln", createUniqueName()
+				+ "@email.org", "xvXV12°§");
+		PreparedStatement ps = DatabaseConnection.getInstance().prepare(
+				"UPDATE users SET dob=NOW() WHERE id=?");
+		ps.setInt(1, id);
+		ps.execute();
+		User assurer = new User(id);
+		for (int i = 0; i < users.length; i++) {
+			assertNotEquals(AssuranceResult.ASSURANCE_SUCCEDED, Notary.assure(
+					assurer, users[i], -1, "test-notary", "2014-01-01"));
+			assertNotEquals(AssuranceResult.ASSURANCE_SUCCEDED, Notary.assure(
+					assurer, users[i], 11, "test-notary", "2014-01-01"));
+			assertEquals(AssuranceResult.ASSURANCE_SUCCEDED, Notary.assure(
+					assurer, users[i], 10, "test-notary", "2014-01-01"));
+			assertNotEquals(AssuranceResult.ASSURANCE_SUCCEDED, Notary.assure(
+					assurer, users[i], 10, "test-notary", "2014-01-01"));
+		}
 	}
 }
