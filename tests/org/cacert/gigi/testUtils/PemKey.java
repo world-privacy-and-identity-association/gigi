26adba2db53b0a4cb7e965219e51175845407be2
Felix Dörre <felix@dogcraft.de>
2014-07-23 04:11:43 +0200

    Shitty incompatible priv-key boundaries.
    e.g. mac openssl produces (-----BEGIN RSA PRIVATE KEY-----)

--- tests/org/cacert/gigi/testUtils/PemKey.java
+++ tests/org/cacert/gigi/testUtils/PemKey.java
@@ -10,9 +10,9 @@ import java.util.Base64;
 public class PemKey {
 	public static PrivateKey parsePEMPrivateKey(String privKeyPEM) throws NoSuchAlgorithmException,
 		InvalidKeySpecException {
-		privKeyPEM = privKeyPEM.replace("-----BEGIN PRIVATE KEY-----", "").replace("\n", "");
+		privKeyPEM = privKeyPEM.replaceAll("-----BEGIN (RSA )?PRIVATE KEY-----", "").replace("\n", "");
 		// Remove the first and last lines
-		privKeyPEM = privKeyPEM.replace("-----END PRIVATE KEY-----", "");
+		privKeyPEM = privKeyPEM.replaceAll("-----END (RSA )?PRIVATE KEY-----", "");
 
 		// Base64 decode the data
 		byte[] encoded = Base64.getDecoder().decode(privKeyPEM);
