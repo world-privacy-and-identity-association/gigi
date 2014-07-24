9bccb7c4e98e832a8fd3f1ab06ecb05dfd59357d
Janis Streib <janis@dogcraft.de>
2014-07-24 22:32:43 +0200

    ADD: Default mail display

--- src/org/cacert/gigi/pages/account/MailOverview.java
+++ src/org/cacert/gigi/pages/account/MailOverview.java
@@ -2,18 +2,15 @@ package org.cacert.gigi.pages.account;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.cacert.gigi.EmailAddress;
 import org.cacert.gigi.Language;
 import org.cacert.gigi.User;
-import org.cacert.gigi.database.DatabaseConnection;
 import org.cacert.gigi.output.Form;
 import org.cacert.gigi.output.Outputable;
 import org.cacert.gigi.pages.LoginPage;
@@ -25,29 +22,19 @@ public class MailOverview extends Page {
 
 	public MailOverview(String title) {
 		super(title);
-		t = new MailTable("res", "us");
+		t = new MailTable("us");
 	}
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		final User us = LoginPage.getUser(req);
 		Language lang = Page.getLanguage(req);
-		int id = us.getId();
-		try {
-			PreparedStatement ps = DatabaseConnection.getInstance().prepare(
-				"SELECT * from `email` WHERE `memid`=? AND `deleted`=0");
-			ps.setInt(1, id);
-			ResultSet rs = ps.executeQuery();
-			HashMap<String, Object> vars = new HashMap<>();
-			vars.put("mailData", t);
-			vars.put("res", rs);
-			vars.put("us", us.getEmail());
-			vars.put("addForm", new MailAddForm(req, us));
-			vars.put("manForm", new MailManagementForm(req));
-			getDefaultTemplate().output(resp.getWriter(), lang, vars);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		HashMap<String, Object> vars = new HashMap<>();
+		vars.put("mailData", t);
+		vars.put("us", us);
+		vars.put("addForm", new MailAddForm(req, us));
+		vars.put("manForm", new MailManagementForm(req));
+		getDefaultTemplate().output(resp.getWriter(), lang, vars);
 	}
 
 	@Override
@@ -68,50 +55,50 @@ public class MailOverview extends Page {
 	}
 
 	private class MailTable implements Outputable {
-		private String mails, userMail;
+		private String user;
 
-		public MailTable(String mails, String userMail) {
-			this.mails = mails;
-			this.userMail = userMail;
+		public MailTable(String user) {
+			this.user = user;
 		}
 
 		@Override
 		public void output(PrintWriter out, Language l, Map<String, Object> vars) {
-			try {
-				ResultSet rs = (ResultSet) vars.get(mails);
-				String usM = (String) vars.get(userMail);
-				while (rs.next()) {
-					out.println("<tr>");
-					out.println("<td><input type=\"radio\" name=\"emailid\" value=\"");
-					int mailID = rs.getInt(1);
-					out.print(mailID);
-					out.print("\"/></td>");
-					out.println("<td>");
-					if (rs.getString(7).isEmpty()) {
-						out.print(l.getTranslation("Verified"));
-					} else {
-						out.print(l.getTranslation("Unverified"));
-					}
-					out.print("</td>");
-					out.println("<td>");
-					String address = rs.getString(3);
-					if (usM.equals(address)) {
-						out.print(l.getTranslation("N/A"));
-					} else {
-						out.print("<input type=\"checkbox\" name=\"delid[]\" value=\"");
-						out.print(mailID);
-						out.print("\"/>");
-					}
-					out.print("</td>");
-					out.println("<td>");
-					out.print(address);
-					out.print("</td>");
-					out.println("</tr>");
+			User us = (User) vars.get(user);
+			String usM = us.getEmail();
+			EmailAddress[] emails = us.getEmails();
+
+			for (int i = 0; i < emails.length; i++) {
+				out.println("<tr>");
+				out.println("<td><input type=\"radio\" name=\"emailid\" value=\"");
+				int mailID = emails[i].getId();
+				out.print(mailID);
+				out.print("\" ");
+				if (emails[i].getAddress().equals(us.getEmail())) {
+					out.print("checked=\"yes\"");
 				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+				out.print("/></td>");
+				out.println("<td>");
+				if (emails[i].isVerified()) {
+					out.print(l.getTranslation("Verified"));
+				} else {
+					out.print(l.getTranslation("Unverified"));
+				}
+				out.print("</td>");
+				out.println("<td>");
+				String address = emails[i].getAddress();
+				if (usM.equals(address)) {
+					out.print(l.getTranslation("N/A"));
+				} else {
+					out.print("<input type=\"checkbox\" name=\"delid[]\" value=\"");
+					out.print(mailID);
+					out.print("\"/>");
+				}
+				out.print("</td>");
+				out.println("<td>");
+				out.print(address);
+				out.print("</td>");
+				out.println("</tr>");
 			}
 		}
-
 	}
 }
