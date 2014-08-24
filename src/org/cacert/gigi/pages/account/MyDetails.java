7d143b2edcb0ea7bfe149fd6b77b35ffcf94acf5
Janis Streib <janis@dogcraft.de>
2014-08-24 11:42:35 +0200

    ADD: Contact infromtaion form

--- src/org/cacert/gigi/pages/account/MyDetails.java
+++ src/org/cacert/gigi/pages/account/MyDetails.java
@@ -23,7 +23,9 @@ public class MyDetails extends Page {
         PrintWriter out = resp.getWriter();
         HashMap<String, Object> map = new HashMap<String, Object>();
         MyDetailsForm form = new MyDetailsForm(req, getUser(req));
+        MyListingForm listingForm = new MyListingForm(req, getUser(req));
         map.put("detailsForm", form);
+        map.put("contactMeForm", listingForm);
         getDefaultTemplate().output(out, getLanguage(req), map);
     }
 
@@ -31,6 +33,9 @@ public class MyDetails extends Page {
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         if(req.getParameter("processDetails") != null) {
             MyDetailsForm form = Form.getForm(req, MyDetailsForm.class);
+            form.submit(resp.getWriter(), req);
+        } else if (req.getParameter("processContact") != null) {
+            MyListingForm form = Form.getForm(req, MyListingForm.class);
             form.submit(resp.getWriter(), req);
         }
         super.doPost(req, resp);
---
+++ src/org/cacert/gigi/pages/account/MyListingForm.java
@@ -0,0 +1,66 @@
+package org.cacert.gigi.pages.account;
+
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.cacert.gigi.GigiApiException;
+import org.cacert.gigi.User;
+import org.cacert.gigi.localisation.Language;
+import org.cacert.gigi.output.Form;
+import org.cacert.gigi.output.template.Template;
+import org.cacert.gigi.pages.Page;
+
+public class MyListingForm extends Form {
+
+    private static Template template;
+    
+    static{
+        template = new Template(new InputStreamReader(MyListingForm.class.getResourceAsStream("MyListingForm.templ")));
+    }
+
+    private User target;
+
+    public MyListingForm(HttpServletRequest hsr, User target) {
+        super(hsr);
+        this.target = target;
+    }
+
+    @Override
+    public boolean submit(PrintWriter out, HttpServletRequest req) {
+        if (req.getParameter("listme") != null && req.getParameter("contactinfo") != null) {
+            try {
+                target.setDirectoryListing( !req.getParameter("listme").equals("0"));
+                target.setContactInformation(req.getParameter("contactinfo"));
+                return true;
+            } catch (SQLException e) {
+                new GigiApiException(e).format(out, Page.getLanguage(req));
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void outputContent(PrintWriter out, Language l, Map<String, Object> vars) {
+        try {
+            if (target.wantsDirectoryListing()) {
+                vars.put("selected", "selected");
+                vars.put("notSelected", "");
+                vars.put("activeInfo", target.getContactInformation());
+            } else {
+                vars.put("selected", "");
+                vars.put("notSelected", "selected");
+                vars.put("activeInfo", "");
+            }
+        } catch (SQLException e) {
+            new GigiApiException(e).format(out, l);
+        }
+        template.output(out, l, vars);
+    }
+
+}
