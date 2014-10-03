74b367ff84986a90499bf7421156be5834ca8365
Felix Dörre <felix@dogcraft.de>
2014-10-03 17:59:55 +0200

    Adding debian package

---
+++ debian/cacert-gigi.default.ex
@@ -0,0 +1,10 @@
+# Defaults for cacert-gigi initscript
+# sourced by /etc/init.d/cacert-gigi
+# installed at /etc/default/cacert-gigi by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
