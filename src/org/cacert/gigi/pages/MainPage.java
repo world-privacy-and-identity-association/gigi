ff4409c745ee9a48e1fd105eff2095df1d9abe95
Felix Dörre <felix@dogcraft.de>
2014-11-05 10:38:36 +0100

    UPD: Fill up index pages with old descriptive text.

--- src/org/cacert/gigi/pages/MainPage.java
+++ src/org/cacert/gigi/pages/MainPage.java
@@ -1,11 +1,16 @@
 package org.cacert.gigi.pages;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.cacert.gigi.output.template.Template;
+
 public class MainPage extends Page {
+
+    Template notLog = new Template(MainPage.class.getResource("MainPageNotLogin.templ"));
 
     public MainPage(String title) {
         super(title);
@@ -13,7 +18,11 @@ public class MainPage extends Page {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Access granted.");
+        if (LoginPage.getUser(req) != null) {
+            getDefaultTemplate().output(resp.getWriter(), getLanguage(req), new HashMap<String, Object>());
+        } else {
+            notLog.output(resp.getWriter(), getLanguage(req), new HashMap<String, Object>());
+        }
     }
 
     @Override
