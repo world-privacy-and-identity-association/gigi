85321fef6a9029ac1108742971fedca962a5a5de
Felix Dörre <felix@dogcraft.de>
2014-12-02 01:47:54 +0100

    upd: Remove nasty dependencies ;-)

--- tests/com/lambdaworks/crypto/test/SCryptUtilTest.java
+++ tests/com/lambdaworks/crypto/test/SCryptUtilTest.java
@@ -2,10 +2,13 @@
 
 package com.lambdaworks.crypto.test;
 
-import com.lambdaworks.codec.Base64;
+import java.util.Base64;
+
 import com.lambdaworks.crypto.SCryptUtil;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SCryptUtilTest {
@@ -22,8 +25,8 @@ public class SCryptUtilTest {
 
         assertEquals(5, parts.length);
         assertEquals("s0", parts[1]);
-        Assert.assertEquals(16, Base64.decode(parts[3].toCharArray()).length);
-        assertEquals(32, Base64.decode(parts[4].toCharArray()).length);
+        Assert.assertEquals(16, Base64.getDecoder().decode(parts[3]).length);
+        assertEquals(32, Base64.getDecoder().decode(parts[4]).length);
 
         int params = Integer.valueOf(parts[2], 16);
 
