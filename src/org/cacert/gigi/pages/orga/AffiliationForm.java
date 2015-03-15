133b7fedbff3ce8d5f4eb6d150e204f615862476
Felix Dörre <felix@dogcraft.de>
2015-03-15 09:25:08 +0100

    Fix: only allow affiliating with org when user is assurer.

--- src/org/cacert/gigi/pages/orga/AffiliationForm.java
+++ src/org/cacert/gigi/pages/orga/AffiliationForm.java
@@ -39,7 +39,7 @@ public class AffiliationForm extends Form {
             }
         } else if (req.getParameter("do_affiliate") != null) {
             User byEmail = User.getByEmail(req.getParameter("email"));
-            if (byEmail != null) {
+            if (byEmail != null && byEmail.canAssure()) {
                 o.addAdmin(byEmail, LoginPage.getUser(req), req.getParameter("master") != null);
                 return true;
             }
