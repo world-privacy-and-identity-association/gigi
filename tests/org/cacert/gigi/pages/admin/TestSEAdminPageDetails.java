ebc00097e5d2c2656ac554e8fef7c4214f26935c
Janis Streib <janis@dogcraft.de>
2015-03-22 12:23:22 +0100

    ADD: Basic name test for details form (SE)

---
+++ tests/org/cacert/gigi/pages/admin/TestSEAdminPageDetails.java
@@ -0,0 +1,37 @@
+package org.cacert.gigi.pages.admin;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.cacert.gigi.dbObjects.Group;
+import org.cacert.gigi.pages.admin.support.SupportUserDetailsPage;
+import org.cacert.gigi.testUtils.ClientTest;
+import org.cacert.gigi.testUtils.IOUtils;
+import org.junit.Test;
+
+public class TestSEAdminPageDetails extends ClientTest {
+
+    public TestSEAdminPageDetails() throws IOException {
+        grant(email, Group.SUPPORTER);
+    }
+
+    @Test
+    public void testUserDetailsDisplay() throws MalformedURLException, IOException {
+        String email = createUniqueName() + "@example.com";
+        String fname = "Först";
+        String lname = "Secönd";
+        int id = createVerifiedUser(fname, lname, email, TEST_PASSWORD);
+        URLConnection uc = new URL("https://" + getServerName() + SupportUserDetailsPage.PATH + id).openConnection();
+        uc.addRequestProperty("Cookie", cookie);
+        uc.setDoOutput(true);
+        String res = IOUtils.readURL(uc);
+        assertThat(res, containsString("<input type=\"text\" value=\"" + fname + "\" name=\"fname\">"));
+        assertThat(res, containsString("<input type=\"text\" value=\"" + lname + "\" name=\"lname\">"));
+        assertThat(res, containsString(email));
+    }
+}
