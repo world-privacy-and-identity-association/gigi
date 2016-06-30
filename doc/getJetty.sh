875b5e9651498a0cd8e0001c0742ba843e47cad0
Felix Dörre <felix@dogcraft.de>
2016-06-30 12:58:00 +0200

    updating update script to jetty-9.2.16.v20160407
    Change-Id: I9f1221ba227c3ba582d0135dd2577604b219d8ce

--- doc/getJetty.sh
+++ doc/getJetty.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 JETTY=C:/jars/jetty-distribution-9.1.0.RC0/org.eclipse.jetty.project
 
 pushd ../lib/jetty/org/eclipse/jetty
@@ -6,7 +7,7 @@ rm -fR *
 
 pushd $JETTY
 
-git checkout refs/tags/jetty-9.2.1.v20140609
+git checkout refs/tags/jetty-9.2.16.v20160407
 popd
 
 
