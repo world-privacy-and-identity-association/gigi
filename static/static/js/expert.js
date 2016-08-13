52bad029da3feab0c54d0a84cac4d2c29d0b3297
Felix Dörre <felix@dogcraft.de>
2016-08-13 13:31:16 +0200

    fix: hide non-activated domain pingconfigs
    Change-Id: I8215ddc94dcb66788868f581604608b3c8c50343

--- static/static/js/expert.js
+++ static/static/js/expert.js
@@ -17,6 +17,19 @@
 		if(expert !== null) {
 			expert.onchange = (function(expert){return function(){showExpert(expert.checked)}})(expert);
 		}
+		$(".panel-activatable").map(function() {
+			var panel = $(this);
+			var refresh = function(){
+				if(this.checked){
+					panel.find(".panel-body").removeClass("hidden");
+				}else{
+					panel.find(".panel-body").addClass("hidden");
+				}
+			};
+			panel.find(".panel-heading [type=\"checkbox\"]").map(refresh);
+			panel.find(".panel-heading [type=\"checkbox\"]").click(refresh);
+			return this.id;
+		});
 	}
 	(function(oldLoad) {
 		if (oldLoad == undefined) {
