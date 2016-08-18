c04dbcca9dc292e74c013ff7de1d6c8adb072ccf
Felix Dörre <felix@dogcraft.de>
2016-08-18 19:54:46 +0200

    add: test case for domain deletion
    Change-Id: I91c89833a8ac736065a4a093e284db725f8dee95

--- tests/org/cacert/gigi/pages/account/TestDomain.java
+++ tests/org/cacert/gigi/pages/account/TestDomain.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.net.URLEncoder;
 
+import org.cacert.gigi.dbObjects.Domain;
 import org.cacert.gigi.pages.account.domain.DomainOverview;
 import org.cacert.gigi.testUtils.ClientTest;
 import org.junit.Test;
@@ -27,6 +28,21 @@ public class TestDomain extends ClientTest {
     @Test
     public void testHighFinancialValue() throws IOException {
         assertNotNull(addDomain(cookie, "google.com"));
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        String domain = uniq + ".de";
+        assertNull(addDomain(cookie, domain));
+        Domain d0 = Domain.searchUserIdByDomain(domain);
+        assertNull(executeBasicWebInteraction(cookie, DomainOverview.PATH, "delete=" + d0.getId(), 0));
+        // double delete
+        assertNotNull(executeBasicWebInteraction(cookie, DomainOverview.PATH, "delete=" + d0.getId(), 0));
+        // re-add
+        assertNull(addDomain(cookie, domain));
+        Domain d1 = Domain.searchUserIdByDomain(domain);
+        assertNotEquals(d0.getId(), d1.getId());
+        assertNull(executeBasicWebInteraction(cookie, DomainOverview.PATH, "delete=" + d1.getId(), 0));
     }
 
     public static String addDomain(String session, String domain) throws IOException {
