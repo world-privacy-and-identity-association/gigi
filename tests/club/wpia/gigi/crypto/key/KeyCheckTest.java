376ad64d4bafc4b2db6990604758addf8fabbd3c
Benny Baumann <BenBE1987@gmx.net>
2017-10-19 01:25:59 +0200

    add: initial class for performing arbitrary checks to validate public keys
    Change-Id: Ia7813913b1f5922747ddba4af9a21e4fbaf07c9e

---
+++ tests/club/wpia/gigi/crypto/key/KeyCheckTest.java
@@ -0,0 +1,36 @@
+package club.wpia.gigi.crypto.key;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.PublicKey;
+
+import org.junit.Test;
+
+import sun.security.util.DerValue;
+import sun.security.x509.X509Key;
+import club.wpia.gigi.GigiApiException;
+import club.wpia.gigi.util.PEM;
+
+public class KeyCheckTest {
+
+    public static PublicKey pkFromString(String pub) throws GeneralSecurityException, IOException {
+        byte[] data = PEM.decode("PUBLIC KEY", pub);
+        DerValue der = new DerValue(data);
+        PublicKey key = X509Key.parse(der);
+
+        return key;
+    }
+
+    @Test
+    public void testNullKey() {
+        try {
+            KeyCheck.checkKey(null);
+            fail("Providing a null key should fail!");
+        } catch (GigiApiException gae) {
+            assertTrue(true);
+        }
+    }
+
+}
