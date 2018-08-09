6693ce661f3def2bdfffbffe3b10ba25dc26970e
INOPIAE <m.maengel@inopiae.de>
2018-08-09 16:34:48 +0200

    add: make sure org admin cannot delete domain from org account
    Only an Org RA Agent should be able to delete a domain from an
    organisation account
    
    Change-Id: I2617f5e75afaea3a877036b4aa29d66abaefa3b6

--- tests/club/wpia/gigi/testUtils/OrgTest.java
+++ tests/club/wpia/gigi/testUtils/OrgTest.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 
 import club.wpia.gigi.GigiApiException;
 import club.wpia.gigi.dbObjects.Country;
+import club.wpia.gigi.dbObjects.Country.CountryCodeType;
 import club.wpia.gigi.dbObjects.Group;
 import club.wpia.gigi.dbObjects.Organisation;
-import club.wpia.gigi.dbObjects.Country.CountryCodeType;
+import club.wpia.gigi.dbObjects.User;
 
 public class OrgTest extends ClientTest {
 
@@ -20,5 +21,11 @@ public class OrgTest extends ClientTest {
     public Organisation createUniqueOrg() throws GigiApiException {
         Organisation o1 = new Organisation(createUniqueName(), Country.getCountryByCode("DE", CountryCodeType.CODE_2_CHARS), "pr", "city", "test@example.com", "", "", u);
         return o1;
+    }
+
+    public User createOrgAdmin(Organisation o) throws GigiApiException {
+        User ua = User.getById(createVerificationUser("testworker", "testname", createUniqueName() + "@testdom.com", TEST_PASSWORD));
+        o.addAdmin(ua, u, true);
+        return ua;
     }
 }
